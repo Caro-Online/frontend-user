2c003d3429dd3b717a0efa340efa27b98430d1d0
viethoang093 <viethoang.hcmus@gmail.com>
2020-12-12 22:21:08 +0700

    init board game (20x20)

--- src/domain/game/components/BoardGame/BoardGame.js
+++ src/domain/game/components/BoardGame/BoardGame.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import Square from './Square'
+import './BoardGame.css'
+
+const boardSize = 20;
 
 export default function BoardGame() {
+    const renderSquare = (i) => {
+        return (
+            <Square
+                key={i}
+                index={i}
+            />
+        );
+    }
+
+    const renderBoardRow = (i) => {
+        let row = []
+        for (let t = 0; t < boardSize; t++) {
+            row.push(renderSquare(i * 19 + t))
+        }
+        return (
+            <div key={i} className="board-row">
+                {row}
+            </div>
+        );
+    }
+
+    const renderBoard = () => {
+        let board = [];
+        for (let t = 0; t < boardSize; t++) {
+            board.push(renderBoardRow(t));
+        }
+        return board;
+    }
     return (
-        <div style={{ height: "90vh", backgroundColor: "blue" }} >
-            Board Game
+        <div className="board-container" >
+            <div className="board">
+                {renderBoard()}
+            </div>
         </div>
     )
 }
--- src/domain/game/pages/GamePage.js
+++ src/domain/game/pages/GamePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AllUser from '../components/AllUser/AllUser'
 import BoardGame from '../components/BoardGame/BoardGame'
 import Chat from '../components/Chat/Chat'
 import History from '../components/History/History'
@@ -6,15 +7,18 @@ import UserInfo from '../components/UserInfo/UserInfo'
 
 export default function GamePage() {
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="row">
-                <div className="col-lg-8 col-md-8">
+                <div className="col-lg-7 col-md-7">
                     <BoardGame />
                 </div>
-                <div className="col-lg-4 col-md-4 d-flex flex-column justify-content-between">
+                <div className="col-lg-3 col-md-3 d-flex flex-column justify-content-between">
                     <UserInfo />
                     <History />
                     <Chat />
+                </div>
+                <div className="col-lg-2 col-md-2">
+                    <AllUser />
                 </div>
             </div>
         </div>
