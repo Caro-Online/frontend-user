f109d84b8944963f1241d8495d2aaad730edde84
viethoang093 <viethoang.hcmus@gmail.com>
2020-12-16 21:26:38 +0700

    firebase config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API = "https://react-caro-online.herokuapp.com"
-export const API = 'http://localhost:4000';
+export const API = "https://react-caro-online.herokuapp.com"
+export const API1 = 'http://localhost:4000';
